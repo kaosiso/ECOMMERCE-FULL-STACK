34428a55ce9d4f2708b7de038512d9cfd4001828
Kaosiso <kaosisochukwum@gmail.com>
2025-03-24 15:53:06 +0800

    final commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from "cors";
+import "dotenv/config.js";
+import { connect } from 'mongoose';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
+import productRouter from './routes/ProductRoute.js';
+import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
+
+
+// App Config
+const app = express()
+const port = process.env.port || 4000
+connectDB()
+connectCloudinary()
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
+
+// api endpoints
+app.use('/api/user', userRouter)
+app.use('/api/product' , productRouter)
+app.use('/api/cart', cartRouter)
+app.use('/api/order',orderRouter)
+
+
+app.get('/', (req,res)=>{
+    res.send("API Working")
+})
+
+app.listen(port, ()=> console.log("Server is running on port: " + port )
+)
